22b19b225f057d12431af1b8434f228cff733498
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-06-29 17:27:55 -0300

    Ajustes Degrader tela Login

--- atividadeClone/AtividadeClone/src/theme/colors.js
+++ atividadeClone/AtividadeClone/src/theme/colors.js
@@ -1,6 +1,15 @@
-export default {
-    gradiente:["#0ff","white","#0ff"],
-    primary: "#8fdab0",
-    secondary: "#141568",
-    tertiary: "black",
-  };
+export default {  
+  primary: "#00ffff",
+  secondary: "#120a8f",
+  tertiary: "#fff"
+}
+
+
+
+
+// export default {
+//     gradiente:["#0ff","white","#0ff"],
+//     primary: "#8fdab0",
+//     secondary: "#141568",
+//     tertiary: "black",
+//   };
